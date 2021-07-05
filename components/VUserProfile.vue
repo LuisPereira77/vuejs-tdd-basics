b743d46d6876853f0786b98797feb8a8d98eaa5e
Luis PEreira <luis.pereira.moz@gmail.com>
2021-07-05 22:07:20 +0200

    feat: add userView base tests

---
+++ components/VUserProfile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  props: {
+    user: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+  },
+})
+</script>
+
+<style></style>
---
+++ pages/UserView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <VUserSearchForm />
+    <VUserProfile :user="user" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import VUserSearchForm from '@/components/VUserSearchForm.vue'
+import VUserProfile from '@/components/VUserProfile.vue'
+export default defineComponent({
+  components: { VUserSearchForm, VUserProfile },
+  data: () => ({
+    user: {
+      name: '',
+    },
+  }),
+})
+</script>
+
+<style></style>
