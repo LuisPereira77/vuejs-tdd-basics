e189caa4cf092eb51d87ed82232d2ba39941154a
Luis PEreira <luis.pereira.moz@gmail.com>
2021-07-09 19:10:30 +0200

    feat: add final userView tests; add userProfile content & tests;

--- components/VUserProfile.vue
+++ components/VUserProfile.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <div class="user-profile">
+    <img
+      id="user-profile__avatar"
+      class="rounded-full w-4"
+      :src="user.avatar_url"
+    />
+    <div id="user-profile__name" class="font-bold">
+      {{ user.name }}
+    </div>
+    <div id="user-profile__bio" class="">
+      {{ user.bio }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
--- pages/UserView.vue
+++ pages/UserView.vue
@@ -1,21 +1,27 @@
 <template>
   <div>
-    <VUserSearchForm />
+    <VUserSearchForm @submitted="searchUser" />
     <VUserProfile :user="user" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
+import { mapState } from 'vuex'
 import VUserSearchForm from '@/components/VUserSearchForm.vue'
 import VUserProfile from '@/components/VUserProfile.vue'
 export default defineComponent({
   components: { VUserSearchForm, VUserProfile },
-  data: () => ({
-    user: {
-      name: '',
+  computed: {
+    ...mapState({
+      user: 'user',
+    }),
+  },
+  methods: {
+    searchUser(username: string) {
+      this.$store.dispatch('SEARCH_USER', { username })
     },
-  }),
+  },
 })
 </script>
 
