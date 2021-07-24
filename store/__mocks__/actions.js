369eec5ca9c9ecd35ff2a71098f785c66ac85e99
Luis PEreira <luis.pereira.moz@gmail.com>
2021-07-24 21:42:07 +0200

    feat: create store module actions mock

---
+++ store/__mocks__/actions.js
@@ -0,0 +1,5 @@
+import userFixture from '~/test/unit/fixtures/user'
+
+export default {
+    SEARCH_USER: jest.fn().mockResolvedValue(userFixture)
+}
--- store/actions.js
+++ store/actions.js
@@ -1,5 +1,3 @@
-import userFixture from '../test/unit/fixtures/user'
-
 export default {
-  SEARCH_USER: jest.fn().mockResolvedValue(userFixture)
+  user: {},
 }
