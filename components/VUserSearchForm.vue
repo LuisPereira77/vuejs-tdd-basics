2c8f5523479301b49d10f2839fdedfca9c75e822
Luis PEreira <luis.pereira.moz@gmail.com>
2021-07-24 21:42:54 +0200

    feat: add userSearchForm test & build userSearchForm component

--- components/VUserSearchForm.vue
+++ components/VUserSearchForm.vue
@@ -1,10 +1,19 @@
 <template>
-  <div></div>
+  <form @submit.prevent="$emit('submitted', username)">
+    <input v-model="username" type="text" />
+    <button type="submit">Enviar</button>
+  </form>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
-export default defineComponent({})
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+export default defineComponent({
+  setup() {
+    const username = ref('')
+
+    return { username }
+  },
+})
 </script>
 
 <style></style>
